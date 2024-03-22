4dfd52848b0a260b0cfa62c5833a16bf83824c00
adenafil <nafilie9@gmail.com>
2024-03-22 11:39:40 +0700

    fix: HTTP response check adjusted to 502
    Adjusted the HTTP response check from 200 to 502 to correctly handle the desired response code.

--- helper/always_connect.sh
+++ helper/always_connect.sh
@@ -18,17 +18,17 @@ while true; do
     echo $response
     echo "end"
     
-    # Periksa respons
-    # if [ "$response" == "200" ]; then
-    #     echo "Respons OK (HTTP 200). Lanjutkan..."
-    # else
-    #     echo "Terjadi kesalahan pada respons. Mengaktifkan mode pesawat..."
-    #     enable_airplane_mode
-    #     sleep 10 # Tunda selama 10 detik sebelum menonaktifkan mode pesawat kembali
-    #     echo "Menonaktifkan mode pesawat kembali..."
-    #     disable_airplane_mode
-    # fi
+    Periksa respons
+    if [ "$response" == "502" ]; then
+        echo "Respons OK (HTTP 200). Lanjutkan..."
+    else
+        echo "Terjadi kesalahan pada respons. Mengaktifkan mode pesawat..."
+        # enable_airplane_mode
+        sleep 10 # Tunda selama 10 detik sebelum menonaktifkan mode pesawat kembali
+        # echo "Menonaktifkan mode pesawat kembali..."
+        # disable_airplane_mode
+    fi
     
-    # Tunda eksekusi selama 30 detik sebelum memeriksa kembali
+    Tunda eksekusi selama 30 detik sebelum memeriksa kembali
     sleep 30
 done
