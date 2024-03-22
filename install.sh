808e17ba82f47fcf12223003db3f96dff6aa1730
adenafil <nafilie9@gmail.com>
2024-03-22 11:17:46 +0700

    feat: download gracia repository, extract, and clean up
    Fixes the issue where the installation script didn't download the gracia repository, extract it, and clean up the extracted files.
    
    BREAKING CHANGE: The installation script now requires a writable /root directory.

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
-wget -r -np https://github.com/adenafil/gracia/archive/main.zipcl
+wget -r -np https://github.com/adenafil/gracia/archive/main.zip
+mv /root/github.com/adenafil/gracia/archive/main.zip /root
+rm -rf github.com
+unzip main.zip
+rm -rf main.zip
