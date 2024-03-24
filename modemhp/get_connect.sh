df218d360660ea5e5f52af75fd2d9e59015a5ef3
adenafil <nafilie9@gmail.com>
2024-03-24 13:37:41 +0700

    fix: Improve logic for checking server connection
    Adjusted server checking logic:
    - Changed server URL to https://server.com
    - Reduced connection timeout to 2 seconds
    - Added support for 301 (Moved Permanently) and 302 (Found) HTTP status codes
    - Improved error handling and logging

--- modemhp/get_connect.sh
+++ modemhp/get_connect.sh
@@ -4,18 +4,23 @@ source "$PWD/../helper/airplane_mode.sh"
 
 while true; do
     # Lakukan Curl ke server dan simpan output ke dalam variabel
-    response=$(curl --connect-timeout 5 -s -I server.com)
+    response=$(curl --connect-timeout 2 -s -I https://server.com)
 
     # Periksa apakah Curl berhasil
     if [[ $response == "" ]]; then
-        echo "the server didnt have any response"
+        echo "$(date) : offline"
         get_suitable_host_on_smartphone
-    elif [[ $response == *"HTTP/1.1 200"* ]]; then
-        echo "Sukses curl - Status: 200 OK"
+    elif [[ $response == *"HTTP/1.1 302"* ]]; then
+        echo "$(date) : offline (302)"
+        get_suitable_host_on_smartphone
+    elif [[ $response == *"HTTP/1.1 301"* ]]; then
+        echo "$(date) : online(301)"
+    elif [[ $response == *"HTTP/2 404"* ]]; then
+        echo "$(date) : online(404)"
     fi
 
     # Istirahat sebelum melakukan percobaan lagi
     sleep 5
-    echo "device need taking time to loop again"
+    echo "$(date) : wait for looping"
     sleep 8
 done
